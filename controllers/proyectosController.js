b1fcb68f2cb3579754eb55fbd05700f9785cefe5
nerincon21 <58572706+nerincon21@users.noreply.github.com>
2022-06-16 12:53:28 -0600

    se agregó validación

--- controllers/proyectosController.js
+++ controllers/proyectosController.js
@@ -12,5 +12,25 @@ exports.formularioProyecto = (req, res) => {
 
 exports.nuevoProyecto = (req, res) => {
     // Enviar a la consola lo que el usuario escriba.
-    console.log(req.body);
+    // console.log(req.body);
+
+    // validar que tengamos algo en el input
+    const { nombre } = req.body;
+
+    let errores = [];
+
+    if(!nombre) {
+        errores.push({'texto': 'Agrega un Nombre al Proyecto'})
+    }
+
+    //si hay errores
+    if(errores.length > 0 ){
+        res.render('nuevoProyecto', {
+            nombrePagina : 'Nuevo Proyecto',
+            errores
+        })
+    } else {
+        //No hay errores
+        // Insertar en la BD
+    }
 }
