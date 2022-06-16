dc9e5a0dea4963b14fd6a08781d500441270f485
nerincon21 <58572706+nerincon21@users.noreply.github.com>
2022-06-16 12:34:09 -0600

    Se agregó body parser

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const routes = require('./routes');
 const path = require('path');
+const bodyParser = require('body-parser');
 
 //Crear una app de express
 const app = express();
@@ -13,6 +14,9 @@ app.set('view engine', 'pug');
 
 //añadir la carpeta de las vistas
 app.set('views', path.join(__dirname, '/views'));
+
+//habilitar bodyParser para leer datos del formulario
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.use('/', routes() );
 
